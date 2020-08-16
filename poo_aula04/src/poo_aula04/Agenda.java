cfdec644678a2ac5cd75b7abbd9dd0286ebe54e0
[TJRN-DIS] Clecio Ferreira <f812778@tjrn.jus.br>
2020-08-16 16:23:47 -0300

    first commit

---
+++ poo_aula04/src/poo_aula04/Agenda.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo_aula04;
+
+/**
+ *
+ * @author clecioferreira
+ */
+public class Agenda {
+    	private int dia;
+	private int mes;
+	private String anotacao;
+        
+        public void anote(int d, int m, String nota){
+		dia = d;
+		mes = m;
+		anotacao = nota;
+                validaData();
+	}
+        
+        private void validaData(){
+		if ((dia<1) ||(dia>31) || (mes<1) || (mes>12)){
+			dia = 0;
+			mes = 0;
+			anotacao = "Anotação não inserida devido a data inválida";
+                }
+        }
+        public void mostraAnotacao(){
+		System.out.println(dia+"/"+mes+":"+anotacao);
+	}
+}
+
---
+++ poo_aula04/src/poo_aula04/Poo_aula04.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo_aula04;
+
+/**
+ *
+ * @author clecioferreira
+ */
+public class Poo_aula04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Agenda agenda1 = new Agenda();
+	Agenda agenda2 = new Agenda();
+
+	agenda1.anote(12,10,"Dia da Criança");
+	agenda2.anote(7,15,"Independência do Brasil");
+
+	agenda1.mostraAnotacao();
+	agenda2.mostraAnotacao();
+        
+        // parte do codigo usada como teste para validar o funcionamento
+        // do encapsulamento de atributos e metodos usando as palavras reservadas
+        // private e public
+        /**
+        agenda2.dia=7;
+        agenda2.mes=15;
+        agenda2.anotacao="Independencia do Brasil";
+        agenda2.mostraAnotacao();
+        **/
+    }
+    
+}
